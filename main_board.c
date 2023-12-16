4853f23a9c80362c5a3d19f7c0fbea78bdc47e13
andrei-lazarov <andrei.lazarov@student.upt.ro>
2023-12-16 16:53:10 +0200

    Atypical flag - board progress

--- main_board.c
+++ main_board.c
@@ -17,8 +17,9 @@
 
 const char at_command_simple[] = "AT\r\n";
 const char at_command_csq[] = "AT+CSQ\r\n";
-const char at_command_creg[] = "AT+CREG\r\n";
-const char at_command_cops[] = "AT+COPS\r\n";
+const char at_command_creg_write[] = "AT+CREG=2\r\n";
+const char at_command_creg[] = "AT+CREG?\r\n";
+const char at_command_cops[] = "AT+COPS?\r\n";
 const char at_command_gsn[] = "AT+GSN\r\n";
 const char at_command_gmi[] = "AT+GMI\r\n";
 const char at_command_gmr[] = "AT+GMR\r\n";
@@ -36,7 +37,8 @@ void testLCD()
 	LCD_PIXEL foreground = {0, 255, 0, 0};
 	LCD_PIXEL background = {0, 0, 0, 0};
 	
-	
+	//printf("height: %d\n",LCD_HEIGHT); //272
+	//printf("width: %d\n",LCD_WIDTH); //480
 	for (i = 0; i < LCD_HEIGHT; i++)
 	{
 		for (j = 0; j < LCD_WIDTH / 3; j++)
@@ -53,14 +55,35 @@ void testLCD()
 		}
 	}
 
+	  for (i = 200; i <= 250; i++)
+		{
+			for (j = 50; j <= 150; j++)
+			{
+				DRV_LCD_PutPixel(i, j, 255, 255, 255);
+			}
+		}
+		
+		for (i = 200; i <= 250; i++)
+		{
+			for (j = 330; j <= 430; j++)
+			{
+				DRV_LCD_PutPixel(i, j, 255, 255, 255);
+			}
+		}
+	
 	DRV_LCD_Puts("Hello", 20, 30, foreground, background, TRUE);
 	DRV_LCD_Puts("Hello", 20, 60, foreground, background, FALSE);	
 }
 
 void TouchScreenCallBack(TouchResult* touchData)
 {
-	printf("touched X=%3d Y=%3d\n", touchData->X, touchData->Y);		
-	
+	if(touchData->Y >= 200 && touchData->Y <= 250)
+	{
+		if(touchData->X >= 50 && touchData->X <=150)
+			printf("touched back\n");
+		else if(touchData->X >= 330 && touchData->X <=430)
+			printf("touched next\n");
+	}	
 }
 
 void BoardInit()
@@ -98,19 +121,19 @@ void SendCommand(const char command[])
 
 void GetCommandResponse()
 {
+	
  uint8_t ch;
  BOOLEAN ready = FALSE;
  TIMER_SOFTWARE_reset_timer(my_handler);
  TIMER_SOFTWARE_start_timer(my_handler);
 	
  data.lineCount = 0;
-	
  while ((!TIMER_SOFTWARE_interrupt_pending(my_handler)) && (ready == FALSE))
  {
 	while (DRV_UART_BytesAvailable(UART_3) > 0)
 	{
 	 DRV_UART_ReadByte(UART_3, &ch);
-	 printf("%c", ch);
+	 //printf("%c", ch);
 	 StateMachineReturn = parse(ch);
 	 if ( StateMachineReturn != STATE_MACHINE_NOT_READY)
 	 {
@@ -128,12 +151,7 @@ void ExecuteCommand(const char *command)
 
 uint32_t ExtractData ()
 {
-	uint32_t result= 0;
-	//while(data.data[0][i]!= '\0')
-	//{
-	//	putchar(data.data[0][i]);
-	//	i++;
-	//}
+	uint32_t result = 0;
 	result = 10* (data.data[0][5] - '0') + (data.data[0][6] - '0');
 	return result;
 }
@@ -146,21 +164,16 @@ uint32_t ConvertAsuToDbmw (uint32_t asu )
 
 int CommandResponseValid()
 {
-	// use STATE MACHINE READY
-	if(StateMachineReturn == STATE_MACHINE_READY_OK)
-		return 1;
-	return 0;
+	return (StateMachineReturn == STATE_MACHINE_READY_OK);
 }
 
 
 int main(void)
 {
-	
 	uint32_t rssi_value_asu;
 	uint32_t rssi_value_dbmw; 
-
+	uint8_t i;
 	uint8_t ch;
-
 	BoardInit();
 	testLCD();
 	
@@ -171,46 +184,65 @@ int main(void)
 	TIMER_SOFTWARE_configure_timer(my_timer_handler, MODE_1, 5000, 1);
 	TIMER_SOFTWARE_start_timer(my_timer_handler);
 	
-	printf ("=========== Program started =========== \n");
-	
+
+	printf ("\n=========== Program started =========== \n\n");
+	atypical = 0;
 	SendCommand(at_command_simple);
-	
 	while(1)
 	{
+		DRV_UART_Process();
+		DRV_TOUCHSCREEN_Process();
+		
 		if (TIMER_SOFTWARE_interrupt_pending(my_timer_handler))
 		 {
-			 
+			   atypical = 0;
 				 ExecuteCommand(at_command_csq);
 				 if (CommandResponseValid())
 				 {
-					 
 					 rssi_value_asu = ExtractData();
 					 rssi_value_dbmw = ConvertAsuToDbmw(rssi_value_asu);
 					 
-					 printf("\nGSM Modem signal %u ASU -> %d dBmW\n\n",rssi_value_asu, rssi_value_dbmw);
+					 printf("GSM Modem signal %u ASU -> %d dBmW\n",rssi_value_asu, rssi_value_dbmw);
 				 }
-			 
-			 ExecuteCommand(at_command_csq);
+			   ExecuteCommand(at_command_cops);
+				 if (CommandResponseValid())
+				 {
+					 if(strcmp(data.data[0]+6,"0") == 0)
+						printf("Operator: No operator\n");
+					 else
+						 printf("Operator: %s\n", data.data[0]+6);
+				 }
+				 ExecuteCommand(at_command_creg);
+				 if (CommandResponseValid())
+				 {
+					 if(strcmp(data.data[0]+6,"0") == 0)
+						printf("State of registration: No operator\n");
+					 else
+						 printf("State of registration: ")
+					 for (, data.data[0]+11);
+				 }
+				 atypical = 1;
+			   ExecuteCommand(at_command_gsn);
+				 if (CommandResponseValid())
+				 {
+					 printf("Modem IMEI: %s\n",data.data[0]);
+				 }
+				 ExecuteCommand(at_command_gmi);
+				 if (CommandResponseValid())
+				 {
+					 printf("Modem manufacturer: %s\n",data.data[0]);
+				 }
+				 ExecuteCommand(at_command_gmr);
+				 if (CommandResponseValid())
+				 {
+					 printf("SW Version: %s\n\n",data.data[0]+9);
+				 }
+				 
 				 TIMER_SOFTWARE_clear_interrupt(my_timer_handler);
 		 } 
 	}
-	/*
-	while(1)
-	{
-		DRV_UART_SendByte(UART_3, 'A');
-	//	TIMER_SOFTWARE_Wait(1000);
-	}
-	*/
-	/*
-	while(1)
-	{
-		if (DRV_UART_ReadByte(UART_3, &ch) == OK)
-		{
-			DRV_UART_SendByte(UART_3, ch);
-		}		
-	}
-*/
-	while(1)
+
+	/*while(1)
 	{
 		if (DRV_UART_ReadByte(UART_0, &ch) == OK)
 		{
@@ -230,7 +262,7 @@ int main(void)
 	{
 		DRV_UART_Process();
 		DRV_TOUCHSCREEN_Process();
-	}
+	}*/
 	
 	return 0; 
 }
